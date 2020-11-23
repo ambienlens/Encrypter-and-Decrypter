401a26c152dabe41b8f6f35adfb441fe14a2f5f2
ambientlensation <debayan.majumder121@gmail.com>
2020-11-23 22:26:02 +0530

    Decrypter added for decryption(code unavailable)

--- decrypter.py
+++ decrypter.py
@@ -1,3 +1,5 @@
-from encoder import extractFileName
-from decoder import *
-import os
+#DEBAYAN MAJUMDER 2020
+#Version 1.0
+#This is the driver python script which lets you enter the name of the file,
+#and then outputs the formated and modified file in Export Dir.
+
